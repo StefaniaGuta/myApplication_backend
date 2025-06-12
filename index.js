192bcc38bfc75768e90fe08e79fb18414b73d74c
StefaniaFlorinela <stefania.florinela@yahoo.com>
2025-06-12 16:55:34 +0300

    change index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server is running. Use our API on port: 3000");
+app.get("/", (req, res) => {
+  res.send("Backend is working!");
 });
